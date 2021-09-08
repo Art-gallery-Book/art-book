c9e9921e2148d38208e2fa5e4c4db1b508d38f85
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-09-08 11:47:37 +0300

    filtering post data

--- app/src/main/java/com/artbook401/artbook/MainActivity.java
+++ app/src/main/java/com/artbook401/artbook/MainActivity.java
@@ -1,7 +1,9 @@
 package com.artbook401.artbook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -10,9 +12,18 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Post;
+import com.amplifyframework.datastore.generated.model.User;
+import com.artbook401.artbook.adapters.PostsAdapter;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,12 +34,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ActionBarDrawerToggle mToggle;
 
     private Toolbar mToolbar;
+    String userName;
+    User currentUser;
+    List<Post> postsList = new ArrayList<>();
+    List<String> followingLists = new ArrayList<>();
+    List<Post> filteredPosts = new ArrayList<>();
+    PostsAdapter myAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getUser();
+
 
         mToolbar = (Toolbar) findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
@@ -41,11 +60,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        findViewById(R.id.textView2).setOnClickListener(v ->
-        {
-            Intent intent = new Intent(getApplicationContext() , Profile.class);
-            startActivity(intent);
-        });
+//        findViewById(R.id.textView2).setOnClickListener(v ->
+//        {
+//            Intent intent = new Intent(getApplicationContext() , Profile.class);
+//            startActivity(intent);
+//        });
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navView);
         navigationView.setNavigationItemSelectedListener(this);
@@ -88,11 +107,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return false;
     }
+    public void getUserName(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-//    public void GoProfile(){
-//
-//        Intent intent = new Intent();
-//        startActivity(intent);
-//    }
+        userName = sharedPreferences.getString("userName", "  ");
+        Log.i(TAG, "getUserName: hello again :" + userName);
 
+    }
+
+    public void getUser(){
+        getUserName();
+        Amplify.API.query(ModelQuery.list(User.class , User.NAME.eq(userName)),
+                success -> {
+                    Log.i(TAG, "onCreate: hi queryyyyyyyyyyyyyyyyy" + success.getData());
+                    for (User user:success.getData())
+                    {
+                       followingLists=user.getFollowing();
+                       currentUser=user;
+
+                    }
+                    Log.i(TAG, "success ");
+                    getPosts();
+                },
+                error->{ Log.i(TAG, "error " + error);}
+        );
+
+    }
+
+    public void getPosts(){
+        Amplify.API.query(ModelQuery.list(Post.class),
+                success ->{
+                    for (Post post:success.getData())
+                    {
+                        postsList.add(post);
+                    }
+                    filteringPosts();
+                },
+                error -> {
+                    Log.i(TAG, "error " + error);
+                }
+                );
+    }
+
+    public void filteringPosts(){
+        for(Post post: postsList){
+            if(followingLists.contains(post.getUserId()) || post.getUserId().equals(currentUser.getId())){
+                filteredPosts.add(post);
+            }
+        }
+        myAdapter = new PostsAdapter(filteredPosts);
+        LinearLayoutManager postsManager = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.VERTICAL, false);
+        RecyclerView postsRecycleView = findViewById(R.id.mainRV);
+        runOnUiThread(() -> {
+            postsRecycleView.setLayoutManager(postsManager);
+            postsRecycleView.setAdapter(myAdapter);
+        });
+    }
 }
---
+++ app/src/main/java/com/artbook401/artbook/adapters/MainAdapter.java
@@ -0,0 +1,4 @@
+package com.artbook401.artbook.adapters;
+
+public class MainAdapter {
+}
